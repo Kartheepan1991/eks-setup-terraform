3beae1b574585a407471942af9a03693ba9d3066
Kartheepan1991 <rkartheepan374@gmail.com>
2025-11-06 12:36:41 +0530

    Fix EKS node group security group configuration
    - Create dedicated node security group
    - Add ingress rules from cluster security group to nodes
    - Add ingress rules from nodes to cluster security group
    - Associate node security group with node groups via remote_access
    - This fixes NodeCreationFailure: nodes can now communicate with control plane

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -99,6 +99,67 @@ resource "aws_security_group" "cluster_sg" {
   tags = merge(local.common_tags, { Name = "${var.cluster_name}-eks-cluster-sg" })
 }
 
+# Node Security Group
+resource "aws_security_group" "node_sg" {
+  name        = "${var.cluster_name}-eks-node-sg"
+  description = "Security group for EKS worker nodes"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = merge(local.common_tags, { Name = "${var.cluster_name}-eks-node-sg" })
+}
+
+# Allow nodes to communicate with each other
+resource "aws_security_group_rule" "node_to_node" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.node_sg.id
+  source_security_group_id = aws_security_group.node_sg.id
+  description              = "Allow nodes to communicate with each other"
+}
+
+# Allow pods to communicate with the cluster API Server
+resource "aws_security_group_rule" "node_to_cluster" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.cluster_sg.id
+  source_security_group_id = aws_security_group.node_sg.id
+  description              = "Allow pods to communicate with the cluster API Server"
+}
+
+# Allow cluster to communicate with nodes
+resource "aws_security_group_rule" "cluster_to_node" {
+  type                     = "ingress"
+  from_port                = 1025
+  to_port                  = 65535
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.node_sg.id
+  source_security_group_id = aws_security_group.cluster_sg.id
+  description              = "Allow cluster API to communicate with worker kubelet"
+}
+
+# Allow cluster to communicate with pods running extension API servers on port 443
+resource "aws_security_group_rule" "cluster_to_node_443" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.node_sg.id
+  source_security_group_id = aws_security_group.cluster_sg.id
+  description              = "Allow cluster API to communicate with pods running extension API servers on port 443"
+}
+
 # EKS Cluster
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
@@ -153,6 +214,11 @@ resource "aws_eks_node_group" "main" {
 
   labels = each.value.labels
 
+  # Configure remote access to use the node security group
+  remote_access {
+    source_security_group_ids = [aws_security_group.node_sg.id]
+  }
+
   dynamic "taint" {
     for_each = each.value.taints
     content {
@@ -175,5 +241,9 @@ resource "aws_eks_node_group" "main" {
     aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
+    aws_security_group_rule.node_to_node,
+    aws_security_group_rule.node_to_cluster,
+    aws_security_group_rule.cluster_to_node,
+    aws_security_group_rule.cluster_to_node_443,
   ]
 }
